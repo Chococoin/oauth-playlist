a82c9ae36ce358aa3df3be82bef88a356642b10f
Germán Lugo <german.lugo@bitnibs.com>
2018-12-28 21:19:49 +0100

    lesson 7.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const GithubStrategy = require('passport-github2');
+const keys = require('./keys');
 
 passport.use(
 	new GoogleStrategy({
 	// option for the google strategy
+	clientID: keys.google.clientID,
+	clientSecret: keys.google.clientSecret
 	}), () =>{
 	// passport callback function
 	}
@@ -12,7 +15,10 @@ passport.use(
 
 passport.use(
 	new GithubStrategy({
-	// option for the github strategy	
+	// option for the github strategy
+	clientID: keys.github.clientID,
+	clientSecret: keys.github.clientSecret,	
+	callbackURL: "http://localhost:8000/auth/github/"
 	}), () => {
 	// passpor callback function
 	}
